fb3d01622ef7a64a6d8938ebe8a020276f2db189
Mila289 <mila.krasulina@gmail.com>
2024-11-26 19:41:50 +0700

    Создание выпадающего списка для выбора криптовалюты

--- cripto.py
+++ cripto.py
@@ -8,3 +8,13 @@ class CryptoApp:
     def __init__(self, master):
         self.master = master
         master.title("Криптовалютный курс")
+# "Создание выпадающего списка для выбора криптовалюты"
+
+        self.crypto_var = tk.StringVar()
+        crypto_list = ['bicoin', 'ethereum', 'ripple', 'litecoin','cardano']
+        self.crypto_menu = ttk.Combobox(master, textvariable=self.crypto_var)
+        self.crypto_menu['values'] = crypto_list
+        self.crypto_menu.pack(pady=10)
+
+
+
